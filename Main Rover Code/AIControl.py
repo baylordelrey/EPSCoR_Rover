11ef8e2ed17402e001332058cb6de67458f9a543
baylordelrey <73312444+baylordelrey@users.noreply.github.com>
2021-11-19 13:46:34 -1000

    Add files via upload

---
+++ Main Rover Code/AIControl.py
@@ -0,0 +1,72 @@
+"""
+1. Write a program to control roboclaw motors with a keyboard
+"""
+
+import sys
+import roboclaw
+import serial
+
+# Linux software serial port on Raspberry Pi
+#ttyAMA0 = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+
+# Linux USB serial port on Raspberry Pi
+# ttyUSB0 = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=3.0)
+
+# Windows USB serial port on PC
+#ttyUSB0 = serial.Serial("COM4", baudrate=115200, timeout=3.0)
+
+# Set up roboclaw
+rc = roboclaw.Roboclaw("/dev/ttyS0", 115200)
+rc.Open()
+
+# Get keyboard input
+def getch():
+    # fd = sys.stdin.fileno()
+    # old_settings = termios.tcgetattr(fd)
+    # try:
+    #     tty.setraw(sys.stdin.fileno())
+    ch = (input("What would you like the rover to do?"))
+    # finally:
+    #     termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+# Motor control
+def motor_control(ch):
+    if ch == 'w':
+        rc.ForwardM1(0x80, 64)
+        rc.ForwardM2(0x80, 64)
+        rc.ForwardM1(0x81, 64)
+        rc.ForwardM2(0x81, 64)
+        rc.ForwardM1(0x82, 64)
+        rc.ForwardM2(0x82, 64)
+        print("Moving Forwards")
+    elif ch == 's':
+        rc.BackwardM1(0x80, 64)
+        rc.BackardM2(0x80, 64)
+        rc.BackwardM1(0x81, 64)
+        rc.BackwardM2(0x81, 64)
+        rc.BackwardM1(0x82, 64)
+        rc.BackwardM2(0x82, 64)
+        print("Moving Backwards")
+    # elif char == 'a':
+    #     rc.BackwardM1(address, 64)
+    #     rc.ForwardM2(address, 64)
+    # elif char == 'd':
+    #     rc.ForwardM1(address, 64)
+    #     rc.BackwardM2(address, 64)
+    elif ch == ' ':
+        rc.ForwardM1(0x80, 0)
+        rc.ForwardM2(0x80, 0)
+        rc.ForwardM1(0x81, 0)
+        rc.ForwardM2(0x81, 0)
+        rc.ForwardM1(0x82, 0)
+        rc.ForwardM2(0x82, 0)
+        print("Stopping")
+
+# Main loop
+if __name__ == "__main__":
+    while True:
+        char = getch()
+        motor_control(char)
+        if char == 'q':
+            break
